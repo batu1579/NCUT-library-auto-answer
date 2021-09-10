b59909316e08712fa5d4e41753eee60a8ab3a545
BATU1579 <batu1234567890@outlook.com>
2021-09-10 09:51:15 +0800

    🎈 perf: 取消禁用pip安装时的日志
    安装pip时没有提示而且还时间颇久，不知道程序有没有再运行，容易让人误以为程序卡住了。

--- library/requirements.py
+++ library/requirements.py
@@ -4,7 +4,7 @@
 Author: BATU1579
 Date: 2021-08-12 05:08:15
 LastEditors: BATU1579
-LastEditTime: 2021-08-15 15:54:24
+LastEditTime: 2021-09-04 18:43:19
 Description: 初次运行时安装依赖文件
 '''
 from pip._internal import main
@@ -16,7 +16,7 @@ with open(".\\library\\run_time.json", "r") as fp:
 
 if data["is_first_time"]:
     # -q参数让pip减少输出
-    main(['install', '-q', '-r', 'requirements.txt'])
+    main(['install', '-r', 'requirements.txt'])
     data["is_first_time"] = False
 
 with open(".\\library\\run_time.json", "w+") as fp:
