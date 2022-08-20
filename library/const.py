386b474929f59b3b3aee9f18667e913feaf34563
BATU1579 <batu1234567890@outlook.com>
2022-08-20 20:45:24 +0800

    ✨ feat: 用新方式读取设置

--- library/const.py
+++ library/const.py
@@ -2,53 +2,47 @@
 # coding=utf-8
 '''
 Author: BATU1579
-Date: 2021-08-10 16:49:21
-LastEditors: BATU1579
-LastEditTime: 2021-09-04 18:46:23
+CreateDate: 2021-08-10 16:49:21
+LastEditor: BATU1579
+LastTime: 2022-08-20 20:34:44
+FilePath: \\library\\const.py
 Description: 存储常量
 '''
 from loguru import logger
-from library.use_data import get_json_data
 from sys import stderr
-import library.settings as profile
 
+from library.use_data import get_json_data
+from library import settings
 
 # 日志文件
 LOG = logger
 LOG.remove()
-LOG.add(
-    ".\\logs\\logs-{time}.log",
-    retention=10,
-    encoding="utf-8",
-    level="DEBUG"
-)
+LOG.add(".\\logs\\logs-{time}.log",
+        retention=10,
+        encoding="utf-8",
+        level="DEBUG")
 
 # 读取常量
 
-CONST = profile.script_settings
+DEV_MODE = settings.dev_mode
 
-DEV_MODE        = CONST["dev_mode"]
+DRIVER_PATH = settings.driver_path
+BROWSER = settings.browser
+GH_TOKEN = settings.GitHub_token
+WAIT_TIME = settings.wait_time
+SHOW_WINDOW = settings.show_window
 
-DRIVER_PATH     = CONST["driver_path"]
-BROWSER         = CONST["browser"]
-GH_TOKEN        = CONST["GitHub_token"]
-WAIT_TIME       = CONST["wait_time"]
-SHOW_WINDOW     = CONST["show_window"]
+VERI_CODE_PATH = settings.veri_code_path
+THRESHOLD_VALUE = settings.threshold_value
 
-VERI_CODE_PATH  = CONST["veri_code_path"]
-THRESHOLD_VALUE = CONST["threshold_value"]
+USE_INPUT_VERI = settings.use_input_veri
 
-USE_INPUT_VERI  = CONST["use_input_veri"]
-
-USE_BAIDU_API   = CONST["use_baidu_api"]
-API_TOKEN_PATH  = CONST["api_token_path"]
-RETRY_NUM       = CONST["retry_num"]
+USE_BAIDU_API = settings.use_baidu_api
+API_TOKEN_PATH = settings.api_token_path
+RETRY_NUM = settings.retry_num
 
 # 读取皮肤文件
-THEME = get_json_data(".\\src\\themes\\%s.json" % CONST["theme"])
+THEME = get_json_data(".\\src\\themes\\%s.json" % settings.theme)
 
 # 控制台日志
-LOG.add(
-    stderr,
-    level="DEBUG" if DEV_MODE else "INFO"
-)
+LOG.add(stderr, level="DEBUG" if DEV_MODE else "INFO")
--- library/settings.py
+++ library/settings.py
@@ -2,57 +2,56 @@
 # coding=utf-8
 '''
 Author: BATU1579
-Date: 2021-08-12 04:31:20
-LastEditors: BATU1579
-LastEditTime: 2021-09-04 17:50:13
+CreateDate: 2021-08-12 04:31:20
+LastEditor: BATU1579
+LastTime: 2022-08-20 20:37:04
+FilePath: \\library\\settings.py
 Description: scripts settings
 '''
 
 # TODO 改换成ini格式的文档，用来支持读写
-script_settings = {
 
-    # 开发者模式（输出更详细的日志）
-    "dev_mode": False,
-    
-    # 驱动文件地址（如果没有为空即可，会自动下载匹配的驱动）
-    "driver_path": "",
+# 开发者模式（输出更详细的日志）
+dev_mode = False
 
-    # 本机使用的浏览器（要与所使用的驱动文件对应）
-    #  - edge
-    #  - chrome
-    #  - firefox
-    "browser": "edge",
+# 驱动文件地址（如果没有为空即可，会自动下载匹配的驱动）
+driver_path = ""
 
-    # Github个人令牌（防止github访问限制导致的下载驱动失败）
-    "GitHub_token": "",
+# 本机使用的浏览器（要与所使用的驱动文件对应）
+#  - edge
+#  - chrome
+#  - firefox
+browser = "edge"
 
-    # 每次获取元素时等待的时间（秒）
-    "wait_time": 0.1,
+# Github个人令牌（防止github访问限制导致的下载驱动失败）
+GitHub_token = ""
 
-    # 运行时显示浏览器窗口
-    "show_window": True,
+# 每次获取元素时等待的时间（秒）
+wait_time = 0.1
 
-    # 验证码图片地址
-    "veri_code_path": ".\\src\\img\\veri_code.png",
+# 运行时显示浏览器窗口
+show_window = True
 
-    # 验证码图像阈值
-    "threshold_value": 130,
+# 验证码图片地址
+veri_code_path = ".\\src\\img\\veri_code.png"
 
-    # 手动输入验证码
-    "use_input_veri": True,
+# 验证码图像阈值
+threshold_value = 130
 
-    # 使用百度api识别验证码
-    "use_baidu_api": False,
+# 手动输入验证码
+use_input_veri = True
 
-    # api认证信息存放位置
-    "api_token_path": ".\\api_token.json",
+# 使用百度api识别验证码
+use_baidu_api = False
 
-    # 调用api时重试的次数
-    "retry_num": 3,
+# api认证信息存放位置
+api_token_path = ".\\api_token.json"
 
-    # 使用的皮肤（如果已经登录选择过皮肤则必须与当前的皮肤一致）
-    #  - school: 校园版
-    #  - student: 书生版
-    #  - warrior: 战士版
-    "theme": "student"
-}
+# 调用api时重试的次数
+retry_num = 3
+
+# 使用的皮肤（如果已经登录选择过皮肤则必须与当前的皮肤一致）
+#  - school: 校园版
+#  - student: 书生版
+#  - warrior: 战士版
+theme = "student"
